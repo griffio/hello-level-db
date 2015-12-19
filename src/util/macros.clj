a7e24e287778ce274b21c76691382193b5fe605f
griffio <griffio@users.noreply.github.com>
2015-12-19 18:46:04 +0000

    macro node->chan
    support macro for node style error first callbacks

--- src/util/macros.clj
+++ src/util/macros.clj
@@ -1,9 +1,12 @@
 (ns util.macros)
 
 (defmacro <? [expr]
+"Wraps <! with exception checking. return value or throw error."
   `(util.error/throw-err (cljs.core.async/<! ~expr)))
 
 (defmacro node->chan [f & args]
+  "Call a node function for supporting error first style callbacks.
+  The callback value, error or value or true, is pushed onto the result channel."
   `(let [c# (cljs.core.async/chan)]
      (~f ~@args #(cljs.core.async/onto-chan c# [(or %1 %2 true)]))
      c#))
